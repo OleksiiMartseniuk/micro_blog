d91b6398cec39a7a9eb675330ad7c5136fc80290
alex <martsenyk.l@gmail.com>
2022-03-10 20:23:39 +0200

    add fastapi-users routers

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from fastapi import FastAPI
-from post.api import post_router
-from db import database, metadata, engine
+from tortoise.contrib.fastapi import register_tortoise
+
+from blog.api import blog_router
+from user.api import user_router
+
+from user.settings import auth_backend, fastapi_users
 
 
 app = FastAPI()
 
-metadata.create_all(engine)
-app.state.database = database
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
+app.include_router(fastapi_users.get_register_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
 
+app.include_router(user_router)
+app.include_router(blog_router)
 
-@app.on_event("startup")
-async def startup() -> None:
-    database_ = app.state.database
-    if not database_.is_connected:
-        await database_.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown() -> None:
-    database_ = app.state.database
-    if database_.is_connected:
-        await database_.disconnect()
-
-
-app.include_router(post_router)
+register_tortoise(
+    app,
+    db_url="sqlite://blog.db",
+    modules={"models": ["blog.models", "user.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
