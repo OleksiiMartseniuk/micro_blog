8a634262401228b806d2d5c39968a11f7dc1746a
alex <martsenyk.l@gmail.com>
2022-03-11 23:42:19 +0200

    edit main

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from blog.api import blog_router
-from user.api import user_router
+from blog.routers import blog_router
 
 from user.settings import auth_backend, fastapi_users
 
@@ -11,11 +10,10 @@ app = FastAPI()
 
 app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
 app.include_router(fastapi_users.get_register_router(), prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_verify_router(), prefix="/auth", tags=["auth"])
+# app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
+# app.include_router(fastapi_users.get_verify_router(), prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
 
-app.include_router(user_router)
 app.include_router(blog_router)
 
 register_tortoise(
