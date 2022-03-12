e1fd879b9383ed4c1630e635ec252e67ddca938d
alex <martsenyk.l@gmail.com>
2022-03-12 20:27:16 +0200

    add two method

--- service_base.py
+++ service_base.py
@@ -1,5 +1,6 @@
 from typing import TypeVar, Type, Optional
 
+from fastapi import HTTPException
 from pydantic import BaseModel
 from tortoise import models
 
@@ -23,8 +24,11 @@ class BaseService:
         obj = await self.model.create(**schema.dict(exclude_unset=True), **kwargs)
         return await self.get_schema.from_tortoise_orm(obj)
 
-    async def update(self):
-        pass
+    async def update(self, schema, **kwargs):
+        await self.model.filter(**kwargs).update(**schema.dict(exclude_unset=True))
+        return await self.get_schema.from_queryset_single(self.model.get(**kwargs))
 
-    async def delete(self):
-        pass
+    async def delete(self, **kwargs):
+        obj = await self.model.filter(**kwargs).delete()
+        if not obj:
+            raise HTTPException(status_code=404, detail=f"Object not found")
