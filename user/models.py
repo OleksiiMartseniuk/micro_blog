11374bea7717c12c57126c15fdfe4c4dea62f216
alex <martsenyk.l@gmail.com>
2022-03-13 17:34:06 +0200

    edit field avatar

--- user/models.py
+++ user/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from fastapi_users import models
 from fastapi_users.db import TortoiseBaseUserModel
 from tortoise.contrib.pydantic import PydanticModel
@@ -11,22 +11,18 @@ from blog.models import Post
 
 class User(models.BaseUser):
     username: str
-    avatar: Optional[str]
 
 
 class UserCreate(models.BaseUserCreate):
     username: str
-    avatar: Optional[str]
 
 
 class UserUpdate(models.BaseUserUpdate):
     username: Optional[str]
-    avatar: Optional[str]
 
 
 class UserModel(TortoiseBaseUserModel):
     username = fields.CharField(max_length=50, unique=True)
-    avatar = fields.CharField(max_length=200)
     posts: fields.ForeignKeyRelation[Post]
 
 
