ec306c6484903f6c06474055b956b8fd40d538af
alex <martsenyk.l@gmail.com>
2022-03-17 23:07:07 +0200

    edit field tag

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,5 @@
 from tortoise import fields, models
 
-from user.models import User
-
-
-class Tag(models.Model):
-    """Model Tag"""
-    name = fields.CharField(max_length=30)
-
 
 class Post(models.Model):
     """Model Post"""
@@ -17,14 +10,21 @@ class Post(models.Model):
     publish = fields.DatetimeField(auto_now_add=True)
     # draft | published
     status = fields.CharField(max_length=9, default='published')
-    tag = fields.ManyToManyField('models.Tag')
+    tag: fields.ManyToManyRelation['Tag'] = fields.ManyToManyField(
+        'models.Tag', related_name='posts', through='post_tag'
+    )
 
 
 class Comment(models.Model):
     """Model Comment"""
-    # post: Optional[Post] = ormar.ForeignKey(Post)
     name = fields.CharField(max_length=200)
     email = fields.CharField(max_length=50)
     body = fields.CharField(max_length=255)
     create = fields.DatetimeField(auto_now_add=True)
     active = fields.BooleanField(default=True)
+
+
+class Tag(models.Model):
+    """Model Tag"""
+    name = fields.CharField(max_length=30, unique=True)
+    posts: fields.ManyToManyRelation[Post]
