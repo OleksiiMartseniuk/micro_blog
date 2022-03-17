972a43e057dc238135aec0f6ad911a3274d1ff51
alex <martsenyk.l@gmail.com>
2022-03-17 16:48:27 +0200

    write image

--- blog/endpoint/post.py
+++ blog/endpoint/post.py
@@ -1,9 +1,16 @@
-from fastapi import Depends, APIRouter
+from fastapi import (Depends,
+                     APIRouter,
+                     UploadFile,
+                     File,
+                     Form,
+                     BackgroundTasks,
+                     HTTPException)
 
 from user.auth import get_user
 from user.models import User
 from blog import schemas, service
 from tortoise.contrib.fastapi import HTTPNotFoundError
+from typing import Optional
 
 
 post_router = APIRouter()
@@ -14,9 +21,29 @@ async def get_post(pk: int):
     return await service.post_s.get(id=pk)
 
 
+# add  tag
 @post_router.post('/', response_model=schemas.GetPost)
-async def create_post(post: schemas.CreatePost, user: User = Depends(get_user)):
-    return await service.post_s.create(post, author_id=user.id)
+async def create_post(
+        background_tasks: BackgroundTasks,
+        title: str = Form(...),
+        body: str = Form(...),
+        file: Optional[UploadFile] = File(None),
+        user: User = Depends(get_user)
+        ):
+    if not file:
+        return await service.post_s.create(title=title,
+                                           body=body,
+                                           author_id=user.id)
+    else:
+        file_name = f'media/{user.id}/{file.filename}'
+        if file.content_type == 'image/jpeg':
+            background_tasks.add_task(service.write_image, file_name, file)
+            return await service.post_s.create(title=title,
+                                               body=body,
+                                               image=file_name,
+                                               author_id=user.id)
+        else:
+            raise HTTPException(status_code=418, detail="It isn't image")
 
 
 @post_router.put("/{pk}", responses={404: {"model": HTTPNotFoundError}})
