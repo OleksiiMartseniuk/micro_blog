248a020a0683510950a68da3d9c8d983bdd35dad
alex <martsenyk.l@gmail.com>
2022-03-17 23:06:27 +0200

    add router tag

--- routers.py
+++ routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from blog.endpoint import post, comment
+from blog.endpoint import post, comment, tag
 from user import api
 
 blog_router = APIRouter()
@@ -9,3 +9,4 @@ blog_router = APIRouter()
 blog_router.include_router(api.user_router, tags=["user"])
 blog_router.include_router(post.post_router, prefix="/post", tags=["post"])
 blog_router.include_router(comment.comment_router, prefix="/comment", tags=["comment"])
+blog_router.include_router(tag.tag_router, prefix="/tag", tags=["tag"])
