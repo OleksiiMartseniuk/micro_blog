437dfdd586debbac5d5eaff57718486ae0c237f6
alex <martsenyk.l@gmail.com>
2022-03-18 17:02:50 +0200

    add two method (list_tag, get_tag)

--- blog/endpoint/tag.py
+++ blog/endpoint/tag.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, Depends
 
+from typing import List
+
+from tortoise.contrib.fastapi import HTTPNotFoundError
+
 from blog import schemas, service
 from user.models import User
 from user.auth import get_user
@@ -8,8 +12,18 @@ from user.auth import get_user
 tag_router = APIRouter()
 
 
+@tag_router.get('', response_model=List[schemas.GetTag])
+async def list_tag():
+    return await service.tag_s.list()
+
+
+@tag_router.get('/{pk}', response_model=schemas.GetTag, responses={404: {"model": HTTPNotFoundError}})
+async def get_tag(pk: int):
+    return await service.tag_s.get(id=pk)
+
+
 @tag_router.post('', response_model=schemas.GetTag)
 async def crate_tag(tag: schemas.CreateTag, user: User = Depends(get_user)):
     return await service.tag_s.create(tag)
 
-# add list tag
+
