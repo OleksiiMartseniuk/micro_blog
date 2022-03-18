dda19cf1f4b29a3f43f1de281b6aa9949b59e8d2
alex <martsenyk.l@gmail.com>
2022-03-18 22:51:31 +0200

    add method delete comment

--- blog/endpoint/comment.py
+++ blog/endpoint/comment.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, APIRouter
+from fastapi import APIRouter
+from tortoise.contrib.fastapi import HTTPNotFoundError
 
 from blog import schemas, service
 
@@ -6,7 +7,11 @@ from blog import schemas, service
 comment_router = APIRouter()
 
 
-@comment_router.post('/', response_model=schemas.GetComment)
+@comment_router.post('', response_model=schemas.GetComment)
 async def create_comment(comment: schemas.CreateComment):
-    # Validate email
     return await service.comment_s.create(comment)
+
+
+@comment_router.delete('/{pk}', response_model=schemas.Status, responses={404: {"model": HTTPNotFoundError}})
+async def delete_comment(pk: int):
+    return await service.comment_s.delete(id=pk)
