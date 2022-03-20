b591a17b3f8b984b5a410f5e3a079407400f23fa
alex <martsenyk.l@gmail.com>
2022-03-20 22:04:46 +0200

    edit settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,8 +1,15 @@
 import os
 
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+
 ALGORITHM = "HS256"
 access_token_jwt_subject = "access"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+APPS_MODELS = [
+    "src.blog.models",
+    "src.user.models",
+    "aerich.models"
+]
