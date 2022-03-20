04b13f13ddcfb1219cf93ef06a24301c2b7c638e
alex <martsenyk.l@gmail.com>
2022-03-20 20:09:51 +0200

    add function get_user

--- user/api.py
+++ user/api.py
@@ -1,6 +1,10 @@
 from fastapi import Depends, APIRouter, Request
 from starlette.templating import Jinja2Templates
-from user import schemas, services
+
+from tortoise.contrib.fastapi import HTTPNotFoundError
+
+from user import schemas, services, models
+from user.auth import get_user
 
 
 templates = Jinja2Templates(directory='templates')
@@ -18,3 +22,8 @@ async def google_auth(request: Request):
 async def google_auth(user: schemas.UserCreate):
     user_id, token = await services.google_auth(user)
     return schemas.Token(id=user_id, token=token)
+
+
+@user_router.get('/user/me', response_model=schemas.GetUser, responses={404: {"model": HTTPNotFoundError}})
+async def get_user(user: models.User = Depends(get_user)):
+    return await services.user_s.get(id=user.id)
